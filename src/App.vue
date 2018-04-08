a6d4c857783c4da86f1f2091315b4acf4e76d4a5
Evgeny <Evkoldin@mail.ru>
2018-04-08 17:11:24 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template lang="html">
-
+    <section class="app__blog">
+        <ul class="blog__list">
+            <li class="blog__item">
+                <a href="/" class="blog__link">
+                    <img src="http://placehold.it/150/f66b97" alt="a" class="blog__preview">
+                    <h3 class="blog__heading">natus nisi omnis corporis facere molestiae rerum in</h3>
+                </a>
+            </li>
+            <li class="blog__item">
+                <a href="/" class="blog__link">
+                    <img src="http://placehold.it/150/f66b97" alt="a" class="blog__preview">
+                    <h3 class="blog__heading">natus nisi omnis corporis facere molestiae rerum in</h3>
+                </a>
+            </li>
+            <li class="blog__item">
+                <a href="/" class="blog__link">
+                    <img src="http://placehold.it/150/f66b97" alt="a" class="blog__preview">
+                    <h3 class="blog__heading">natus nisi omnis corporis facere molestiae rerum in</h3>
+                </a>
+            </li>
+            <li class="blog__item">
+                <a href="/" class="blog__link">
+                    <img src="http://placehold.it/150/f66b97" alt="a" class="blog__preview">
+                    <h3 class="blog__heading">natus nisi omnis corporis facere molestiae rerum in</h3>
+                </a>
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
